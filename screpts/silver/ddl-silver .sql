d8ac537da2192653ee486e7aa0ffc32c026a9a04
Mahmoud khaled <72028660+Mahmoud-morsi-0@users.noreply.github.com>
2025-08-03 14:31:56 +0300

    Update ddl-silver .sql

--- screpts/silver/ddl-silver .sql
+++ screpts/silver/ddl-silver .sql
@@ -5,6 +5,8 @@
 -- Each table includes a 'dwh_insert_date' column to track when the data was inserted into the silver layer.
 -- The script ensures no duplicate tables by dropping existing ones before creation.
 
+
+
 IF OBJECT_ID('silver.crm_cust_info','U' ) IS NOT NULL 
 	DROP TABLE silver.crm_cust_info ; 
 CREATE TABLE silver.crm_cust_info(
@@ -15,62 +17,70 @@ CREATE TABLE silver.crm_cust_info(
 	cst_marital_status	NVARCHAR(50),
 	cst_gndr	NVARCHAR(50),
 	cst_create_date DATE ,
-	dwh_insert_date DATETIME2 DEFAULT GETDATE()  -- EXTRA COLUMN TO GET THE DATE AND TIME THAT DATA WILL BE INSERTED 
-); 
+	dwh_insert_date DATETIME2 DEFAULT GETDATE()  --EXTRA COLUMN TO GET THE DATE AND TIME THAT DTA WILL BE INSERTED 
+	); 
 
 IF OBJECT_ID('silver.crm_prd_info','U' ) IS NOT NULL 
 	DROP TABLE silver.crm_prd_info ; 
 CREATE TABLE silver.crm_prd_info(
+	
 	prd_id	INT ,	
 	prd_key	NVARCHAR(50),
-	cat_id NVARCHAR(50), -- added in transformation step
+	cat_id NVARCHAR(50),--- we add this coulmn onn transformation 
 	prd_nm	NVARCHAR(50),
 	prd_cost INT ,
 	prd_line NVARCHAR(50),
-	prd_start_dt	DATE , -- changed from DATETIME
-	prd_end_dt DATE , -- changed from DATETIME
-	dwh_insert_date DATETIME2 DEFAULT GETDATE() -- EXTRA COLUMN TO GET THE DATE AND TIME THAT DATA WILL BE INSERTED
-)
+	prd_start_dt	DATE ,--- we changed it from DATETIME to date because we chande it in the transformation 
+	prd_end_dt DATE ,--- we changed it from DATETIME to date because we chande it in the transformation 
+	dwh_insert_date DATETIME2 DEFAULT GETDATE()--EXTRA COLUMN TO GET THE DATE AND TIME THAT DTA WILL BE INSERTED 
+
+	)
 
 IF OBJECT_ID('silver.crm_sales_details','U' ) IS NOT NULL 
 	DROP TABLE silver.crm_sales_details ; 
 CREATE TABLE silver.crm_sales_details(
-	sls_ord_num	NVARCHAR(50),
-	sls_prd_key	NVARCHAR(50),
-	sls_cust_id	INT ,
-	sls_order_dt	DATE , -- updated data type from INT to DATE
-	sls_ship_dt	DATE ,
-	sls_due_dt	DATE,
-	sls_sales	INT ,
-	sls_quantity	INT ,
-	sls_price INT ,
-	dwh_insert_date DATETIME2 DEFAULT GETDATE() -- EXTRA COLUMN TO GET THE DATE AND TIME THAT DATA WILL BE INSERTED
-)
+		sls_ord_num	NVARCHAR(50),
+		sls_prd_key	NVARCHAR(50),
+		sls_cust_id	INT ,
+		sls_order_dt	date ,---update the data  type from int to date 
+		sls_ship_dt	date  ,
+		sls_due_dt	date,
+		sls_sales	INT ,
+		sls_quantity	INT ,
+		sls_price INT ,
+		dwh_insert_date DATETIME2 DEFAULT GETDATE()--EXTRA COLUMN TO GET THE DATE AND TIME THAT DTA WILL BE INSERTED 
+
+		)
 
 IF OBJECT_ID('silver.erp_CUST_AZ12','U' ) IS NOT NULL 
-	DROP TABLE silver.erp_CUST_AZ12; 
+		DROP TABLE silver.erp_CUST_AZ12; 
 CREATE TABLE silver.erp_CUST_AZ12(
-	CID	NVARCHAR(50),
-	BDATE	DATE ,
-	GEN	NVARCHAR(50),
-	dwh_insert_date DATETIME2 DEFAULT GETDATE() -- EXTRA COLUMN TO GET THE DATE AND TIME THAT DATA WILL BE INSERTED
-)
+		
+		CID		NVARCHAR(50),
+		BDATE	DATE ,
+		GEN		NVARCHAR(50),
+		dwh_insert_date DATETIME2 DEFAULT GETDATE()--EXTRA COLUMN TO GET THE DATE AND TIME THAT DTA WILL BE INSERTED 
+
+		)
+
 
 IF OBJECT_ID('silver.erp_LOC_A101','U' ) IS NOT NULL 
-	DROP TABLE silver.erp_LOC_A101; 
+		DROP TABLE silver.erp_LOC_A101; 
 CREATE TABLE silver.erp_LOC_A101(
-	CID	NVARCHAR(50),
-	CNTRY NVARCHAR(50),
-	dwh_insert_date DATETIME2 DEFAULT GETDATE() -- EXTRA COLUMN TO GET THE DATE AND TIME THAT DATA WILL BE INSERTED
-)
+		CID		NVARCHAR(50),
+		CNTRY NVARCHAR(50),
+		dwh_insert_date DATETIME2 DEFAULT GETDATE()--EXTRA CLOUMN TO GET THE DATE AND TIME THAT DTA WILL BE INSERTED 
+
+		)
 
 IF OBJECT_ID('silver.erp_PX_CAT_G1V2','U' ) IS NOT NULL 
-	DROP TABLE silver.erp_PX_CAT_G1V2; 
+		DROP TABLE silver.erp_PX_CAT_G1V2; 
 CREATE TABLE silver.erp_PX_CAT_G1V2(
-	ID	NVARCHAR(50),
-	CAT	NVARCHAR(50),
-	SUBCAT	NVARCHAR(50),
-	MAINTENANCE NVARCHAR(50),
-	dwh_insert_date DATETIME2 DEFAULT GETDATE() -- EXTRA COLUMN TO GET THE DATE AND TIME THAT DATA WILL BE INSERTED
-)
+		ID	NVARCHAR(50),
+		CAT	NVARCHAR(50),
+		SUBCAT	NVARCHAR(50),
+		MAINTENANCE NVARCHAR(50),
+		dwh_insert_date DATETIME2 DEFAULT GETDATE()--EXTRA CLOUMN TO GET THE DATE AND TIME THAT DTA WILL BE INSERTED 
 
+
+		)
