3c747322158e8db264392677136aa2316c0f9ed7
Mahmoud khaled <72028660+Mahmoud-morsi-0@users.noreply.github.com>
2025-08-03 14:25:46 +0300

    Update ddl-silver .sql

--- screpts/silver/ddl-silver .sql
+++ screpts/silver/ddl-silver .sql
@@ -1,1 +1,76 @@
+-- Description:
+-- This SQL script creates the necessary tables in the 'silver' layer of a Data Warehouse pipeline. 
+-- These tables store cleaned and standardized data from CRM and ERP systems, including customer info, product info, 
+-- sales details, customer birth and gender data, location data, and product category metadata.
+-- Each table includes a 'dwh_insert_date' column to track when the data was inserted into the silver layer.
+-- The script ensures no duplicate tables by dropping existing ones before creation.
+
+IF OBJECT_ID('silver.crm_cust_info','U' ) IS NOT NULL 
+	DROP TABLE silver.crm_cust_info ; 
+CREATE TABLE silver.crm_cust_info(
+	cst_id	INT ,
+	cst_key	NVARCHAR(50),
+	cst_firstname	NVARCHAR(50),
+	cst_lastname	NVARCHAR(50),
+	cst_marital_status	NVARCHAR(50),
+	cst_gndr	NVARCHAR(50),
+	cst_create_date DATE ,
+	dwh_insert_date DATETIME2 DEFAULT GETDATE()  -- EXTRA COLUMN TO GET THE DATE AND TIME THAT DATA WILL BE INSERTED 
+); 
+
+IF OBJECT_ID('silver.crm_prd_info','U' ) IS NOT NULL 
+	DROP TABLE silver.crm_prd_info ; 
+CREATE TABLE silver.crm_prd_info(
+	prd_id	INT ,	
+	prd_key	NVARCHAR(50),
+	cat_id NVARCHAR(50), -- added in transformation step
+	prd_nm	NVARCHAR(50),
+	prd_cost INT ,
+	prd_line NVARCHAR(50),
+	prd_start_dt	DATE , -- changed from DATETIME
+	prd_end_dt DATE , -- changed from DATETIME
+	dwh_insert_date DATETIME2 DEFAULT GETDATE() -- EXTRA COLUMN TO GET THE DATE AND TIME THAT DATA WILL BE INSERTED
+)
+
+IF OBJECT_ID('silver.crm_sales_details','U' ) IS NOT NULL 
+	DROP TABLE silver.crm_sales_details ; 
+CREATE TABLE silver.crm_sales_details(
+	sls_ord_num	NVARCHAR(50),
+	sls_prd_key	NVARCHAR(50),
+	sls_cust_id	INT ,
+	sls_order_dt	DATE , -- updated data type from INT to DATE
+	sls_ship_dt	DATE ,
+	sls_due_dt	DATE,
+	sls_sales	INT ,
+	sls_quantity	INT ,
+	sls_price INT ,
+	dwh_insert_date DATETIME2 DEFAULT GETDATE() -- EXTRA COLUMN TO GET THE DATE AND TIME THAT DATA WILL BE INSERTED
+)
+
+IF OBJECT_ID('silver.erp_CUST_AZ12','U' ) IS NOT NULL 
+	DROP TABLE silver.erp_CUST_AZ12; 
+CREATE TABLE silver.erp_CUST_AZ12(
+	CID	NVARCHAR(50),
+	BDATE	DATE ,
+	GEN	NVARCHAR(50),
+	dwh_insert_date DATETIME2 DEFAULT GETDATE() -- EXTRA COLUMN TO GET THE DATE AND TIME THAT DATA WILL BE INSERTED
+)
+
+IF OBJECT_ID('silver.erp_LOC_A101','U' ) IS NOT NULL 
+	DROP TABLE silver.erp_LOC_A101; 
+CREATE TABLE silver.erp_LOC_A101(
+	CID	NVARCHAR(50),
+	CNTRY NVARCHAR(50),
+	dwh_insert_date DATETIME2 DEFAULT GETDATE() -- EXTRA COLUMN TO GET THE DATE AND TIME THAT DATA WILL BE INSERTED
+)
+
+IF OBJECT_ID('silver.erp_PX_CAT_G1V2','U' ) IS NOT NULL 
+	DROP TABLE silver.erp_PX_CAT_G1V2; 
+CREATE TABLE silver.erp_PX_CAT_G1V2(
+	ID	NVARCHAR(50),
+	CAT	NVARCHAR(50),
+	SUBCAT	NVARCHAR(50),
+	MAINTENANCE NVARCHAR(50),
+	dwh_insert_date DATETIME2 DEFAULT GETDATE() -- EXTRA COLUMN TO GET THE DATE AND TIME THAT DATA WILL BE INSERTED
+)
 
